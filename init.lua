27e0964db4fe11c123af7ef4b53c23f0a4e52c87
tacigar <ig.lhound@gmail.com>
2016-06-03 02:12:54 +0900

    Update and Fix

--- init.lua
+++ init.lua
@@ -19,6 +19,8 @@ local function combine_plant_and_size(plantname, size)
   return plantname.."_"..tostring(size)
 end
 
+local tree_grow_flag = default.can_grow and default.grow_sapling
+
 local plants = {}
 minetest.after(0, function()
   for nodename, _ in pairs(minetest.registered_nodes) do
@@ -35,7 +37,9 @@ minetest.register_craftitem("speedgrow:speedgrow", {
   description = "Speedgrow",
   inventory_image = "speedgrow.png",
   stack_max = 1,
+  
   on_use = function(itemstack, user, pointed_thing)
+    if pointed_thing.under == nil then return end
     local pos = pointed_thing.under
     local node = minetest.get_node(pos)
     if minetest.get_item_group(node.name, "plant") > 0 then
@@ -45,11 +49,15 @@ minetest.register_craftitem("speedgrow:speedgrow", {
       minetest.set_node(pos, {name = newnode_name})
     elseif minetest.get_item_group(node.name, "seed") > 0 then
       local modname, pn = split_seed_plant_name(node.name)
-      print(modname, pn)
       local plantname = modname..":"..pn
       local size = plants[plantname]
       local newnode_name = combine_plant_and_size(plantname, size)
       minetest.set_node(pos, {name = newnode_name})
+    elseif tree_grow_flag
+    and minetest.get_item_group(node.name, "sapling") > 0 then
+      if default.can_grow(pos) then
+	default.grow_sapling(pos)
+      end
     end
     return nil
   end,

dc8b9947ccd37acd6f8bd8caa17b3de6a6adc3e2
tacigar <ig.lhound@gmail.com>
2016-09-17 19:58:38 +0900

    [UPDATE] Complete speedgrow

--- init.lua
+++ init.lua
@@ -13,10 +13,11 @@ end
 -- from a seed node name.
 local function get_plantname_from_seedname(nodename)
 	local modname, plantname = string.match(nodename, "(.+):seed_(.+)")
+	if (not modname) or (not plantname) then
+		return nil
+	end
 	return modname .. plantname
 end
-
-
 
 -- plants_maxsize represents a table that contains base plant names
 -- and its maximum size.
@@ -38,29 +39,49 @@ minetest.after(0, function()
 	end
 end)
 
+-- grow_up grows up a plant that exists at position pos.
+local function grow_up(pos, plantname)
+	local maxsize = plants_maxsize[plantname]
+	local new_nodename = plantname .. tostring(maxsize)
+
+	minetest.set_node(pos, {name = new_nodename})
+end
+
 -- can_grow_tree represents whether trees can be grown by this mod.
 local can_grow_tree =
 	default.can_grow and default.grow_sapling
 
+-- on_use is a callback that called when speedgrow is used.
 local function on_use(itemstack, user, pointed_thing)
 	if pointed_thing.under == nil then
-		return
+		return nil
 	end
 	local position = pointed_thing.under
 	local node = minetest.get_node(position)
 
 	if minetest.get_item_group(node.name, "plant") > 0 then
-		local plantname, size = split_plant_name(node.name)
+		local plantname, size = split_plantname(node.name)
 		if (not plantname) or (not size) then
-			return
+			return nil
 		end
-		grow_up(plantname)
+		grow_up(position, plantname)
 
 	elseif minetest.get_item_group(node.name, "seed") > 0 then
+		local plantname = get_plantname_from_seedname(node.name)
+		if not plantname then
+			return nil
+		end
+		grow_up(position, plantname)
 
+	elseif can_grow_tree and minetest.get_item_group(node.name, "sapling") > 0 then
+		if default.can_grow(position) then
+			default.grow_sapling(position)
+		end
 	end
+	return nil
 end
 
+-- register a definition of speedgrow.
 minetest.register_craftitem("speedgrow:speedgrow", {
 	description      = "Speedgrow",
 	inventory_image  = "speedgrow.png",
@@ -68,29 +89,4 @@ minetest.register_craftitem("speedgrow:speedgrow", {
 	stack_max        = 1,
 	groups           = {speedgrow = 1},
 	on_use           = on_use,
-
-
-	on_use = function(itemstack, user, pointed_thing)
-		if pointed_thing.under == nil then return end
-		local pos = pointed_thing.under
-		local node = minetest.get_node(pos)
-		if minetest.get_item_group(node.name, "plant") > 0 then
-			local plantname, size = split_plant_name(node.name)
-			local newsize = plants[plantname]
-			local newnode_name = combine_plant_and_size(plantname, newsize)
-			minetest.set_node(pos, {name = newnode_name})
-		elseif minetest.get_item_group(node.name, "seed") > 0 then
-			local modname, pn = split_seed_plant_name(node.name)
-			local plantname = modname..":"..pn
-			local size = plants[plantname]
-			local newnode_name = combine_plant_and_size(plantname, size)
-			minetest.set_node(pos, {name = newnode_name})
-		elseif tree_grow_flag
-			and minetest.get_item_group(node.name, "sapling") > 0 then
-				if default.can_grow(pos) then
-					default.grow_sapling(pos)
-				end
-			end
-			return nil
-		end,
-	})
+})
